b4ca5f4ec56d8dd88469b0d5af455b8ff68f78c5
wiseaidev <oss@wiseai.dev>
2025-05-22 16:54:44 +0300

    fix: open external links in a new tab

--- theme/tabs.js
+++ theme/tabs.js
@@ -72,4 +72,12 @@ document.addEventListener('DOMContentLoaded', () => {
             changeTab(container, name);
         }
     }
+
+    const links = document.querySelectorAll("a[href^='http']");
+    for (const link of links) {
+      if (!link.href.includes(location.hostname)) {
+        link.setAttribute("target", "_blank");
+        link.setAttribute("rel", "noopener noreferrer");
+      }
+    }
 });
